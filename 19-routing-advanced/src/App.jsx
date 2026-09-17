d1cd2ddae5a5f2c5078e75a90886a66770233793
Shashank <onlyshashankshekhar2007@gmail.com>
2026-09-17 11:58:28 +0530

    Basic Routing Application

---
+++ 19-routing-advanced/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Product from './pages/Product';
+import Notfound from './Notfound';
+import Men from './pages/Men';
+import Women from './pages/Women';
+import Courses from './pages/Courses';
+import CourseDetail from './pages/CourseDetail';
+import Navbar2 from './components/Navbar2';
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black text-white'>
+      <Navbar />
+      <Navbar2 />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/courses' element={<Courses />} />
+        <Route path='/courses/:id' element={<CourseDetail/>} />
+        <Route path='/product' element={<Product />}>
+          <Route path='men' element={<Men />} />
+          <Route path='women' element={<Women />} />
+        </Route>
+        <Route path='*' element={<Notfound />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ 19-routing-advanced/src/components/Navbar2.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Navbar2 = () => {
+    let navigate = useNavigate();
+    return (
+        <div className='py-2 px-5 bg-cyan-700'>
+            <button
+                onClick={() => {
+                    navigate('/')
+                }}
+                className='font-medium bg-emerald-800 px-5 py-2 m-2 rounded cursor-pointer active:scale-95'>Return to home page</button>
+
+            <button
+                onClick={() => {
+                    navigate(-1)
+                }}
+                className='font-medium bg-emerald-800 px-5 py-2 m-2 rounded cursor-pointer active:scale-95'>Back</button>
+
+            <button
+                onClick={() => {
+                    navigate(+1)
+                }}
+                className='font-medium bg-emerald-800 px-5 py-2 m-2 rounded cursor-pointer active:scale-95'>Next</button>
+        </div>
+    )
+}
+
+export default Navbar2
